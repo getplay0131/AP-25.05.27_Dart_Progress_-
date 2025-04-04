5bfbcd42fc278775bb3b3fdd3c1d777509cd71fe
seong_hyun_notebook <getplay0131@gmail.com>
2025-04-04 22:35:55 +0900

    25.04.04 2nd Check

--- basic_Project/Data_Analyze/step2_basic_functions.dart
+++ basic_Project/Data_Analyze/step2_basic_functions.dart
@@ -4,7 +4,17 @@ void main() {
   // 샘플 데이터
   // TODO: 월별 판매량 리스트 만들기 (1단계와 동일한 데이터)
   List<num> monthSellCountList = [10, 12.5, 15, 17, 20, 30.5];
-  num result = sum(monthSellCountList);
+  num sumResult = sum(monthSellCountList);
+  num aveResult = average(monthSellCountList);
+  num maxResult = findMax(monthSellCountList);
+  num minResult = findMin(monthSellCountList);
+
+  // 2-D: 함수를 호출하고 결과 출력하기
+  // TODO: 위에서 만든 함수들을 호출하여 결과 출력하기
+  // TODO: 예: "합계: [결과]", "평균: [결과]" 형식으로 출력
+  print(
+    '합계 : $sumResult , 평균 : $aveResult , 최대값 : $maxResult , 최솟값 : $minResult',
+  );
 }
 
 // 2-A: 합계(sum) 함수 만들기
@@ -12,26 +22,45 @@ void main() {
 // 함수 시그니처: int sum(List<int> numbers)
 num sum(List<num> numList) {
   num total = 0;
-  for (var nums in numList) {
-    total += nums;
+  // for (var nums in numList) {
+  //   total += nums;
+  // }
+  // print('$total');
+  if (numList.isEmpty) {
+    return 0;
   }
-  print('$total');
+  // list.reduce : 값들을 하나씩 가져와 누적 => for-in반복문과 역할 비슷슷
+  total = numList.reduce((a, b) => a + b);
+  // print('total : $total');
   return total;
 }
-  // 2-B: 평균(average) 함수 만들기
-  // TODO: 숫자 리스트를 받아 평균값을 반환하는 함수 작성하기
-  // 함수 시그니처: double average(List<int> numbers)
-  // 힌트: 평균 = 합계 / 개수, 이 때 합계 함수를 재사용할 수 있어요
 
-  // 2-C: 최대값(max)과 최소값(min) 함수 만들기
-  // TODO: 숫자 리스트를 받아 최대값을 반환하는 함수 작성하기
-  // 함수 시그니처: int findMax(List<int> numbers)
-  // TODO: 숫자 리스트를 받아 최소값을 반환하는 함수 작성하기
-  // 함수 시그니처: int findMin(List<int> numbers)
+// 2-B: 평균(average) 함수 만들기
+// TODO: 숫자 리스트를 받아 평균값을 반환하는 함수 작성하기
+// 함수 시그니처: double average(List<int> numbers)
+// 힌트: 평균 = 합계 / 개수, 이 때 합계 함수를 재사용할 수 있어요
+double average(List<num> numList) {
+  return sum(numList) / numList.length;
+}
 
-  // 2-D: 함수를 호출하고 결과 출력하기
-  // TODO: 위에서 만든 함수들을 호출하여 결과 출력하기
-  // TODO: 예: "합계: [결과]", "평균: [결과]" 형식으로 출력
+// 2-C: 최대값(max)과 최소값(min) 함수 만들기
+// TODO: 숫자 리스트를 받아 최대값을 반환하는 함수 작성하기
+// 함수 시그니처: int findMax(List<int> numbers)
+num findMax(List<num> numList) {
+  num maxValue = numList.reduce((a, b) => a > b ? a : b);
+  // print('maxValue : $maxValue');
+  return maxValue;
+}
+
+// TODO: 숫자 리스트를 받아 최소값을 반환하는 함수 작성하기
+// 함수 시그니처: int findMin(List<int> numbers)
+num findMin(List<num> numList) {
+  num minValue = numList.reduce((a, b) => a > b ? b : a);
+  // print('minValue : $minValue');
+  return minValue;
+}
+
+
 
   // 힌트: 함수 내에서 초기값을 설정하고, 반복문으로 모든 요소 처리하기
   // 힌트: 최대값 찾기 - 첫 번째 요소를 최대값으로 초기화 후, 더 큰 값 나오면 갱신
--- basic_Project/Data_Analyze/step3_function_parameters.dart
+++ basic_Project/Data_Analyze/step3_function_parameters.dart
@@ -3,26 +3,40 @@
 void main() {
   // 샘플 데이터
   // TODO: 몇 개의 샘플 리스트 만들기
+  List<num> list1 = [1, 2, 3, 4, 5];
+  List<num> list2 = [10, 20, 30, 40, 50];
+  List<num> list3 = [1.2, 3, 5.1, 10, 7.5];
 
-  // 3-A: 포지셔널 파라미터 활용하기
-  // TODO: 두 숫자를 받아 합을 반환하는 함수 만들기
-  // 함수 시그니처: int add(int a, int b)
-  // TODO: 함수 호출하고 결과 출력하기
-
-  // 3-B: 옵셔널 파라미터 추가하기
-  // TODO: 리스트와 가중치(옵셔널)를 받아 가중 합계를 계산하는 함수 만들기
-  // 함수 시그니처: double weightedSum(List<int> numbers, [double weight = 1.0])
-  // TODO: 함수를 다양한 방식으로 호출하고 결과 출력하기 (가중치 있/없음)
-
-  // 3-C: 네임드 파라미터로 함수 만들기
-  // TODO: 리스트를 받아 조건에 따라 분석하는 함수 만들기
-  // 함수 시그니처: double analyze({required List<int> data, required String operation, bool printResult = true})
-  // 힌트: operation 값에 따라 "sum", "average", "max", "min" 중 하나의 연산 수행
-
-  // 3-D: 파라미터 기본값 설정하기
-  // TODO: 네임드 파라미터와 기본값을 활용한 함수 호출 테스트
-
-  // 힌트: 옵셔널 파라미터는 대괄호 []로 감싸고 기본값을 지정할 수 있어요
-  // 힌트: 네임드 파라미터는 중괄호 {}로 감싸고 required 키워드로 필수 여부 지정할 수 있어요
-  // 힌트: 네임드 파라미터 호출 예 - analyze(data: myList, operation: "sum")
+  print(addAnotherNum(1));
+  print(addAnotherNum(15));
 }
+
+// 3-C: 네임드 파라미터로 함수 만들기
+// TODO: 리스트를 받아 조건에 따라 분석하는 함수 만들기
+// 함수 시그니처: double analyze({required List<int> data, required String operation, bool printResult = true})
+// 힌트: operation 값에 따라 "sum", "average", "max", "min" 중 하나의 연산 수행
+
+// 3-D: 파라미터 기본값 설정하기
+// TODO: 네임드 파라미터와 기본값을 활용한 함수 호출 테스트
+
+// 힌트: 옵셔널 파라미터는 대괄호 []로 감싸고 기본값을 지정할 수 있어요
+// 힌트: 네임드 파라미터는 중괄호 {}로 감싸고 required 키워드로 필수 여부 지정할 수 있어요
+// 힌트: 네임드 파라미터 호출 예 - analyze(data: myList, operation: "sum")
+
+// 3-A: 포지셔널 파라미터 활용하기
+// TODO: 두 숫자를 받아 합을 반환하는 함수 만들기
+// 함수 시그니처: int add(int a, int b)
+// TODO: 함수 호출하고 결과 출력하기
+num add(num a, num b) {
+  if (a == 0 && b == 0) {
+    return 0;
+  }
+  print(a + b);
+  return a + b;
+}
+
+// 3-B: 옵셔널 파라미터 추가하기
+// TODO: 리스트와 가중치(옵셔널)를 받아 가중 합계를 계산하는 함수 만들기
+// 함수 시그니처: double weightedSum(List<int> numbers, [double weight = 1.0])
+// TODO: 함수를 다양한 방식으로 호출하고 결과 출력하기 (가중치 있/없음)
+num addAnotherNum(num a, [num b = 10]) => a + b;
